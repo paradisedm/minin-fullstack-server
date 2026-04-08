494f8df5eecea82983ed2d631396c72d1f07686b
ParadiseDM <ParadiseDMX@gmail.com>
2026-04-08 12:33:35 +0300

    Обновил файл

--- app.js
+++ app.js
@@ -1,29 +1,33 @@
+import cors from 'cors';
+
 const express = require('express');
-const config = require('config');
-const path = require('path');
+// const config = require('config');
+// const path = require('path');
 const mongoose = require('mongoose');
 
 const app = express();
 
 app.use(express.json({extended: true}))
-
+app.use(cors());
 app.use('/api/auth', require('./routes/auth.routes'))
 app.use('/api/link', require('./routes/link.routes'))
 app.use('/t/', require('./routes/redirect.routes'))
 
-if (process.env.NODE_ENV === 'production') {
-	app.use('/', express.static(path.join(__dirname, 'client', 'build')))
+// if (process.env.NODE_ENV === 'production') {
+// 	app.use('/', express.static(path.join(__dirname, 'client', 'build')))
 
-	app.get('*', (req, res) => {
-		res.sendFile(path.resolve(__dirname, 'client', 'build', 'index.html'))
-	})
-}
+// 	app.get('*', (req, res) => {
+// 		res.sendFile(path.resolve(__dirname, 'client', 'build', 'index.html'))
+// 	})
+// }
 
-const PORT = config.get('port') || 5000;
+// const PORT = config.get('port') || 5000;
+const PORT = process.env.PORT || 5000;
 
 async function start() {
  try {
-	await mongoose.connect(config.get('mongoUri'));
+	// await mongoose.connect(config.get('mongoUri'));
+	await mongoose.connect(process.env.MONGO_URI);
 	app.listen(PORT, () => console.log(`App запустился на порте ${PORT}`));
  } catch (e) {
 	console.log('Server error', e.message)
